43d19144b550e8a8190ecb8adcb1ee0ea4032277
shika-i <ishikaawachat@gmail.com>
2020-11-14 16:01:48 -0600

    able to edit Settings page

--- frontend/src/pages/Settings.js
+++ frontend/src/pages/Settings.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
+import profilepic from './profilepic.jpg';
 export default class Settings extends Component {
   render() {
     return (
+      
       <div>
-        <h3>Settings</h3> 
+        <h3>Settings</h3>
+        <h3>hewwo</h3> 
+        <img src={profilepic} alt="yeesh"></img>
       </div>
     );
   }
