70e10e333e1d258e9208b07f42e67ae070e706f5
shika-i <ishikaawachat@gmail.com>
2020-11-15 14:52:33 -0600

    centered content on settings page

--- frontend/src/pages/Settings.js
+++ frontend/src/pages/Settings.js
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
 import profilepic from './profilepic.jpg';
+import "./Settings.css";
 export default class Settings extends Component {
   render() {
     return (
       
-      <div>
-        <h3>Settings</h3>
-        <h3>hewwo</h3> 
-        <img src={profilepic} alt="yeesh"></img>
+      <div style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+      }}>
+        <img src={profilepic} alt="profpic"></img>
+        <h2>[User's Name]</h2>
       </div>
+
     );
   }
 }
