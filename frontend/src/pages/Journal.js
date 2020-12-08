2cb4733b22d7ac99ba1711d96596bbc406b0c0f8
@sm202073 <sana.madhavan20@gmail.com>
2020-12-08 17:05:29 -0600

    added journal page emotion buttons

--- frontend/src/pages/Journal.js
+++ frontend/src/pages/Journal.js
@@ -1,10 +1,36 @@
 import React, { Component } from "react";
+import frustrated from './frustrated.png';
+import content from './content.png';
+import excited from './excited.png';
+import InputGroup from 'react-bootstrap/InputGroup';
+import { Form, FormControl, Button } from "react-bootstrap";
+import { StyleSheet, Text, View, Image } from 'react';
+
 export default class Journal extends Component {
   render() {
     return (
-      <div>
-        <h3>Journal</h3> 
+
+          <div>
+          <br></br>
+          <br></br>
+          <br></br>
+          <h3>Journal</h3> 
+
+        <div>
+        <InputGroup>
+         <InputGroup.Prepend>
+           <InputGroup.Text>Log your feelings!</InputGroup.Text>
+            </InputGroup.Prepend>
+           <FormControl as="textarea" aria-label="Log" />
+        </InputGroup>
+        </div>
+        <br></br>
+        <Button variant="success">Submit</Button>{' '}
       </div>
+        
+
+      
+    
     );
   }
 }
